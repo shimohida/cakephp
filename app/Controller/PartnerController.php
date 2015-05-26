74d42f64334df22164c1512b53335839d33057e4
shimohida <border_jin@yahoo.co.jp>
2015-05-26 16:16:49 +0900

    PartnerContlloer
    PartnerContlloerを追加しました

---
+++ app/Controller/PartnerController.php
@@ -0,0 +1,39 @@
+<?php
+
+	APP::uses('AppController', 'Controller');
+
+
+
+	class PartnerController extends AppController {
+
+
+		public $uses = array('UserTB');
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

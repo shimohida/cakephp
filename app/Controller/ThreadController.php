7d35fb77d2e784fd5cbf18a76e8763c53cf76516
shimohida <border_jin@yahoo.co.jp>
2015-05-28 14:57:39 +0900

    thread(contlloer)  &   thread1

--- app/Controller/ThreadController.php
+++ app/Controller/ThreadController.php
@@ -4,67 +4,85 @@ App::uses('AppController', 'Controller');
 class ThreadController extends AppController {
     public $uses = array('UserTb','PartnerWork1Tb');
 
-    public function index(){
-
-    }
-
 
     public function thread1() {
 
-        //ログイン関係
-        $req = $this->request->data;   //post
-
-        if(isset($req)){
-
-            if($req['name'] != "" && $req['pass'] != ""){
-
-                //送られてきた値で検索をかける
-                $where = array('conditions' => array('UserTb.name =' => $req['name'] , 'UserTb.pass =' => $req['pass']));
-                $datas =$this->UserTb->find('all' , $where);
-                $this->set('login_data', $datas);
-                var_dump($datas);
-
-                //結果により分岐させる
-                if(count($datas) != 1){
-                    $this->redirect(array('controller' =>'less','action'=>'index', '?' => array('mode' => 2) ));
-                }else{
-                    // スレッド表示のための、データベース検索
-                    $datas = $this->PartnerWork1Tb->find('all', array('order' => array('PartnerWork1Tb.id' => 'desc')));
-                    $this->set('db_data', $datas);
-                }
-
-            }else {
-                $this->redirect(array('controller' =>'less','action'=>'index', '?' => array('mode' => 1) ));
-            }
-
-        }else{
-            $this->redirect(array('controller' =>'less','action'=>'index', '?' => array('mode' => 1) ));
-        }
-
+    	// スレッド表示のための、データベース検索
+    	$datas = $this->PartnerWork1Tb->find('all', array('order' => array('PartnerWork1Tb.id' => 'desc')));
+    	$this->set('db_data', $datas);
 
     }//thread1 終了
 
+
     public function thread2() {
     }
+
 
     public function run_regist(){
         // INSERT
         if($this->request->data['title'] != '' && $this->request->data['detail'] != ''){
             $this->PartnerWork1Tb->create();
-            $this->PartnerWork1Tb->save(array('user_tb_id' => 6,'thread' => $this->request->data['title'], 'detail' => $this->request->data['detail']));
+            $this->PartnerWork1Tb->save(array('user_tb_id' => 1,'thread' => $this->request->data['title'], 'detail' => $this->request->data['detail']));
         }
         else
-            echo '空白が入っていたのでスレッド作成ができませんでした。';
-
         $this->redirect("/thread/thread1");
     }
+
 
     public function delete_user(){
-        // DELETE
-        if(isset($this->request->data['id'])){
- 			$this->PartnerWork1Tb->delete($this->request->data['id']);
+
+    	//削除関係
+    	$req = $this->request->data;   //post
+
+        if(isset($req)){
+
+        	if($req['user_name'] != "" && $req['delete_id'] != ""){
+
+        		//送られてきた値で２か所に検索をかける
+        		//UserTb を検索」
+        		$where = array('conditions' => array('UserTb.name =' => 'shimohida' ));
+                $user =$this->UserTb->find('all' , $where);
+
+                //PartnerWork1Tb を検索
+                $where = array('conditions' => array('PartnerWork1Tb.id =' => $req['delete_id'] ));
+                $thread =$this->PartnerWork1Tb->find('all' , $where);
+
+
+        		//結果により分岐させる
+        		if($user[0]['UserTb']['id'] != $thread[0]['PartnerWork1Tb']['user_tb_id']){
+        			//スレッドのIDとuser が不一致
+        			$this->redirect(array('controller' =>'thread','action'=>'thread1', '?' => array('delete' => 2) ));
+        		}else{
+        			// スレッドの削除を行う
+        			$this->PartnerWork1Tb->delete($req['delete_id']);
+        			$this->redirect(array('controller' =>'thread','action'=>'thread1', '?' => array('delete' => 3) ));
+        		}
+
+        	}else {
+        		$this->redirect(array('controller' =>'thread','action'=>'thread1', '?' => array('delete' => 1) ));
+        	}
+
+        }else{
+        	$this->redirect(array('controller' =>'thread','action'=>'thread1', '?' => array('delete' => 1) ));
         }
-        $this->redirect("/thread/thread1");
-    }
-}
+    } //delete_user 終了
+
+} //クラスの終了
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- app/View/Thread/thread1.ctp
+++ app/View/Thread/thread1.ctp
@@ -1,10 +1,25 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-	<meta charset="UTF-8">
-	<title>データベース一覧</title>
-</head>
-<body>
+
+<?php if(isset($_GET['delete'])){
+		if ($_GET['delete'] == 1){ ?>
+
+		<h2>エラーが発生しました</h2>
+
+	<?php	}else if($_GET['delete'] == 2){ ?>
+
+		<h2>「UserID」が違うため削除できません！</h2>
+
+	<?php	}else if($_GET['delete'] == 3){ ?>
+
+		<h2>スレッドは削除されました</h2>
+
+	<?php } ?>
+
+
+
+
+
+<?php } //isset ?>
+
 	<button onclick="location.href='thread2'">新規スレッド</button>
 	<br/>
 	<br/>
@@ -23,12 +38,11 @@
 			<td><?php echo $value['UserTb']['name']; ?></td>
 			<td>
 				<form action="/kensyu/cakephp/thread/delete_user" method="POST">
-					<input type="hidden" name="id" value="<?php echo $value['PartnerWork1Tb']['id']; ?>">
+					<input type="hidden" name="user_name" value="shimohida">
+					<input type="hidden" name="delete_id" value="<?php echo $value['PartnerWork1Tb']['id']; ?>">
 					<input type="submit" value="削除">
 				</form>
 			</td>
 		</tr>
 	<?php endforeach; ?>
 	</table>
-</body>
-</html>

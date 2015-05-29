c5a13082e6508d4f9e2e3857eb0b5d506f53a455
fujita0928 <utida0928@gmail.com>
2015-05-29 10:14:46 +0900

    login bug
    login bug

--- app/Controller/LoginController.php
+++ app/Controller/LoginController.php
@@ -21,10 +21,11 @@ class LoginController extends AppController {
     	if(isset($login['name']) && isset($login['pass'])){
     		//送られてきた値で検索をかける
     		$where = array('conditions' => array('UserTb.id' => $login['name'] , 'UserTb.pass' => $login['pass']));
-    		$datas =$this->UserTb->find('all', $where);
+    		$datas = $this->UserTb->find('all', $where);
+    		$this->set('db_data', $datas);
 
 			if(count($datas) > 0){
-				$_SESSION['login_id'] = 1;
+				$_SESSION['login_id'] = $login['name'];
     			$this->redirect(array('controller' =>'thread','action'=>'thread1', '?' => array('user_id' => $login['name'])));
 			}
     	}

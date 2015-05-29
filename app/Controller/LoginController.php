f4c891418af97c35113ff9ca07cf4c530c0bbe9c
fujita0928 <utida0928@gmail.com>
2015-05-29 17:47:27 +0900

    login
    login

--- app/Controller/LoginController.php
+++ app/Controller/LoginController.php
@@ -7,12 +7,12 @@ class LoginController extends AppController {
     public function login(){
     	session_start();
 
-    	if(!isset($_GET['mode']) && isset($_SESSION['login_id']) && $_SESSION['login_id'] === 1){
-    		$this->redirect(array('controller' =>'thread','action'=>'thread1'));
-    	}
-
     	if(isset($_GET['mode']) && $_GET['mode'] == 'logout'){
     		unset($_SESSION['login_id']);
+    	}
+
+    	if(isset($_SESSION['login_id'])){
+    		$this->redirect(array('controller' =>'thread','action'=>'thread1'));
     	}
 
     	// post
@@ -26,7 +26,7 @@ class LoginController extends AppController {
 
 			if(count($datas) > 0){
 				$_SESSION['login_id'] = $login['name'];
-    			$this->redirect(array('controller' =>'thread','action'=>'thread1', '?' => array('user_id' => $login['name'])));
+    			$this->redirect(array('controller' =>'thread', 'action'=>'thread1', '?' => array('user_id' => $login['name'])));
 			}
     	}
     }
